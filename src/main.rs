3b1ef8494795cbe2cf025604b29cdb2dd0aa4fa4
Shafi <shafiyaftali001@gmail.com>
2024-01-17 13:26:45 -0500

    feat: if and else satatement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+fn add(num_one: i32, num_two: i32) -> i32{
+    return num_one + num_two; 
+}
 fn main() {
     // variables
-    let my_name = "Shafi";
+    // let my_name = "Shafi";
     
-
+    let add_two_numbers = add(2, 4);
+    println!("{:?}", add_two_numbers);
 }
