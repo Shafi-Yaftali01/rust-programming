cee029d7432fce407508572a5a48d96f1beb7e75
Shafi <shafiyaftali001@gmail.com>
2024-01-17 16:47:47 -0500

    feat: used borrowing method

--- src/structures.rs
+++ src/structures.rs
@@ -6,14 +6,29 @@ struct BankAccount {
 
 }
 
+fn print_balance(account: BankAccount) {
+  println!("{:?}", account.balance);
+
+}
+
+fn print_verified(account: &BankAccount) {
+  println!("{:?}", account.verified);
+}
+
+
 fn main() {
   let my_account = BankAccount {
     balance: 333,
     verified: true
   };
 
-  println!("{:?}", my_account.balance);
-  println!("{:?}", my_account.verified);
-  
+  // println!("{:?}", my_account.balance);
+  // println!("{:?}", my_account.verified);
+
+  // Borrowing
+  print_balance(&my_account);
+  print_verified(&my_account);
+
+
 
 }
