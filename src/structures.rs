138e3249be35d68ed840819eaed7b988d9a6a7c1
Shafi <shafiyaftali001@gmail.com>
2024-01-18 12:35:19 -0500

    feat: finished working with structures

--- src/structures.rs
+++ src/structures.rs
@@ -15,12 +15,18 @@ fn print_verified(account: &BankAccount) {
   println!("{:?}", account.verified);
 }
 
-
+fn is_verified(account: &BankAccount) -> Result<bool, bool> {
+  match account.verified {
+    true => Ok(true),
+    false => Err(false)
+  };
+}
 fn main() {
   let my_account = BankAccount {
     balance: 333,
     verified: true
   };
+  let verification_status = is_verified(&my_account).expect("Unable to unwrap result.");
 
   // println!("{:?}", my_account.balance);
   // println!("{:?}", my_account.verified);
@@ -28,7 +34,7 @@ fn main() {
   // Borrowing
   print_balance(&my_account);
   print_verified(&my_account);
-
+  println!("{:?}", verification_status);
 
 
 }
